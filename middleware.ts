5a27e21d6e4fa4c957d058172b7eb80cc876824d
uddisharma <uddibhardwaj08@gmail.com>
2024-09-04 15:49:53 +0530

    middleware

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+
+    const { pathname } = request.nextUrl;
+
+    if (token && (pathname === '/login' || pathname === '/register')) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+
+    if (!token && pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        '/',
+        '/dashboard/:path*',
+        '/login',
+        '/register'
+    ]
+};
